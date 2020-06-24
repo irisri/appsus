93aad238b2bda1bdbd00972545585be2be7a16ab
Asia Vaks <asiavaks@gmail.com>
2020-06-24 20:28:03 +0300

    add email preview

--- js/apps/misterEmail/cmps/email-preview.cmp.js
+++ js/apps/misterEmail/cmps/email-preview.cmp.js
@@ -1,4 +1,5 @@
 
+import longText from './long-text.cmp.js';
 
 export default {
     props: ['email'],
@@ -6,22 +7,32 @@ export default {
         <li class="email-preview clean-list align-center text-center">
             <!-- <img v-bind:src="" /> -->
             <h5>{{email.name}}</h5>
-            <pre>{{email.subject}} {{email.body}} {{email.sentAt}}</pre>
+            <pre>{{email.subject}} {{trimBody}} {{sentAt}}</pre>
+            <!-- <long-text v-bind:txt="email.body"></long-text> -->
             <!-- <p>{{showCurrency}}</p> -->
             <!-- <router-link :to="'/email/' + email.id">Details</router-link> -->
         </li>
     `,
     data() {
         return{
-            name: '',
-            address: '',
-            subject: '',
-            body: '',
-            isRead: false,
             sentAt: '',
+            trimBody: ''
         }
     },
-
     computed: {
+       
+    },
+    created(){
+        let trimTxt = this.email.body.length > 60 ? this.email.body.substring(0, 60) + "..." : this.email.body
+        this.trimBody = trimTxt;
+        console.log(this.trimBody);
+        let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        let date = new Date(this.email.sentAt);
+        this.sentAt = `${date.getDate()} ${months[date.getMonth()]}`;
+        console.log(this.sentAt);
+        
+    },
+    components: {
+        longText,
     },
 };
